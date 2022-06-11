b2e716387eed842996275e51829180c46ca7fbf5
dear91304526 <dear91304526@gmail.com>
2022-06-11 16:41:50 +0800

    test

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ func main() {
 	srv := &http.Server{
 		Handler:        router,
 		Addr:           "0.0.0.0:23001",
-		ReadTimeout:    60 * time.Second,
-		WriteTimeout:   60 * time.Second,
-		IdleTimeout:    60 * time.Second,
+		ReadTimeout:    time.Duration(60) * time.Second,
+		WriteTimeout:   time.Duration(60) * time.Second,
+		IdleTimeout:    time.Duration(60) * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 
