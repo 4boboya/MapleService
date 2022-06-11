06de95e782b836d80f378717f442c14be73a3167
dear91304526 <dear91304526@gmail.com>
2022-06-11 23:34:34 +0800

    test

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	routes "MapleService/control"
 )
@@ -12,15 +11,16 @@ func main() {
 	fmt.Println("start")
 	router := routes.NewRouter()
 
-	service := &http.Server{
-		Handler:        router,
-		Addr:           "0.0.0.0:23001",
-		ReadTimeout:    60 * time.Second,
-		WriteTimeout:   60 * time.Second,
-		IdleTimeout:    60 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
+	// service := &http.Server{
+	// 	Handler:        router,
+	// 	Addr:           "0.0.0.0:23001",
+	// 	ReadTimeout:    60 * time.Second,
+	// 	WriteTimeout:   60 * time.Second,
+	// 	IdleTimeout:    60 * time.Second,
+	// 	MaxHeaderBytes: 1 << 20,
+	// }
 
-	service.ListenAndServe()
+	// service.ListenAndServe()
+	http.ListenAndServe("0.0.0.0:23001", router)
 	fmt.Println("end")
 }
