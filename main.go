2026207d4d5682644ebd13bf77fdf14acb05f00e
dear91304526 <dear91304526@gmail.com>
2022-06-11 16:17:15 +0800

    test

--- main.go
+++ main.go
@@ -14,10 +14,11 @@ func main() {
 	router := routes.NewRouter()
 
 	srv := &http.Server{
-		Handler:      router,
-		Addr:         "0.0.0.0:23001",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
+		Handler:        router,
+		Addr:           "0.0.0.0:23001",
+		ReadTimeout:    60 * time.Second,
+		WriteTimeout:   60 * time.Second,
+		MaxHeaderBytes: 1 << 20,
 	}
 
 	log.Fatal(srv.ListenAndServe())
